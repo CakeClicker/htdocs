77217f51b9ee47b880ae3a3c751d22b990b10ad8
CakeClicker <l.super@gmx.net>
2023-05-09 15:13:15 +0200

    I just started lol

---
+++ Index.php
@@ -0,0 +1,12 @@
+<?php
+
+?>
+<html>
+    <head>
+        <title>Cakeys Fruity PHP Website</title>
+    </head>
+
+    <ul>
+        <li><a href="Pages/Cakey_Our_Godfather.php">Godfather</a></li>
+    </ul>
+</html>
---
+++ Pages/Cakey_Our_Godfather.php
@@ -0,0 +1,11 @@
+<?php
+
+?>
+<html>
+    <head>
+        <title>Cakeys Fruity PHP Website</title>
+        <h1>Cakey is the best and our Godfather!</h1>
+    </head>
+
+   
+</html>
